8fd887df13021fdc7c3cb942f1a4456944bcfed1
BoE1008 <15871807472@163.com>
2023-10-31 23:54:32 +0800

    feat: 10-31 night 2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        rel="preload"
+        href="/fonts/SanJiLingYunZhangCao-2.ttf"
+        as="font"
+        type="font/ttf"
+      />
       <body>
         <Main />
         <NextScript />
